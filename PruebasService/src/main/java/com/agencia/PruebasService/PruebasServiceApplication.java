deec182eaf63f2487c417d2011539008ff910620
Marcos Ignacio Belli <95348@sistemas.frc.utn.edu.ar>
2024-10-31 13:08:54 -0300

    cambios

---
+++ PruebasService/src/main/java/com/agencia/PruebasService/PruebasServiceApplication.java
@@ -0,0 +1,12 @@
+package com.agencia.PruebasService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebasServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebasServiceApplication.class, args);
+	}
+
+}
